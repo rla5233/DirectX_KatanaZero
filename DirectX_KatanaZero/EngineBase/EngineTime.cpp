015e58db9bc565c1f48b0ab337668b05d3ebfa7e
rla5233 <rla5233@naver.com>
2024-04-08 21:16:28 +0900

    김수원 : EngineBase - Time 파일 추가

---
+++ DirectX_KatanaZero/EngineBase/EngineTime.cpp
@@ -0,0 +1,48 @@
+#include "PreCompile.h"
+#include "EngineTime.h"
+#include <Windows.h>
+
+UEngineTime::UEngineTime()
+{
+	TimeCheckStart();
+}
+
+UEngineTime::~UEngineTime()
+{
+}
+
+//typedef union _LARGE_INTEGER {
+//    struct {
+//        DWORD LowPart;
+//        LONG HighPart;
+//    } DUMMYSTRUCTNAME;
+//    struct {
+//        DWORD LowPart;
+//        LONG HighPart;
+//    } u;
+//    LONGLONG QuadPart;
+//} LARGE_INTEGER;
+
+
+void UEngineTime::TimeCheckStart()
+{
+	// 초당 샐수 있는 양인데.
+	QueryPerformanceFrequency(&Count);
+
+	QueryPerformanceCounter(&PrevTime);
+
+	dCount = static_cast<double>(Count.QuadPart);
+}
+
+float UEngineTime::TimeCheck()
+{
+	QueryPerformanceCounter(&CurTime);
+
+	__int64 Tick = (CurTime.QuadPart - PrevTime.QuadPart);
+
+	// 정밀도를 높이기 위해서 double로 계산한다.
+	double dTick = static_cast<double>(Tick);
+	DeltaTime = dTick / dCount;
+	PrevTime.QuadPart = CurTime.QuadPart;
+	return static_cast<float>(DeltaTime);
+}
