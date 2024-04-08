c55665f643f791b50bfe2365c158c23270363254
rla5233 <rla5233@naver.com>
2024-04-08 21:35:07 +0900

    김수원 : EngineCore - Actor 파일 추가

---
+++ DirectX_KatanaZero/EngineCore/Actor.cpp
@@ -0,0 +1,274 @@
+#include "PreCompile.h"
+#include "Actor.h"
+#include "SceneComponent.h"
+#include "DefaultSceneComponent.h"
+
+std::set<AActor*> AActor::InputActors;
+std::set<AActor*> AActor::PrevInputActors;
+
+AActor::AActor()
+{
+	// UEngineInput
+	// InputActors.insert(this);
+}
+
+AActor::~AActor()
+{
+}
+
+void AActor::InputOff()
+{
+	// map같은 경우에는 pair 이터레이터
+	// set은 정말 편합니다.
+	InputActors.erase(this);
+}
+
+void AActor::InputOn()
+{
+	//std::map<AActor*, int> Test;
+	//Test.insert(std::pair<AActor*, int>(this, 10));
+	InputActors.insert(this);
+}
+
+void AActor::RootCheck()
+{
+	if (nullptr == RootComponent)
+	{
+		RootComponent = CreateDefaultSubObject<UDefaultSceneComponent>("UDefaultSceneComponent");
+	}
+}
+
+void AActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	for (size_t i = 0; i < Components.size(); i++)
+	{
+		Components[i]->SetWorld(GetWorld());
+		Components[i]->BeginPlay();
+	}
+}
+
+void AActor::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+	for (size_t i = 0; i < Components.size(); i++)
+	{
+		if (false == Components[i]->IsActive())
+		{
+			continue;
+		}
+
+		Components[i]->Tick(_DeltaTime);
+	}
+}
+
+void AActor::PushComponent(std::shared_ptr<UActorComponent> _Component, std::string_view _Name)
+{
+
+	_Component->SetActor(this);
+	_Component->SetName(_Name);
+	//_Component->BeginPlay();
+
+	Components.push_back(_Component);
+
+	// shared_ptr을 안전하게 다운캐스팅하는 함수
+	std::shared_ptr<USceneComponent> SceneComponent = std::dynamic_pointer_cast<USceneComponent>(_Component);
+
+	if (nullptr == RootComponent && nullptr != SceneComponent)
+	{
+		// std::shared_ptr을 그냥 포인터로 변경하는 함수
+		RootComponent = SceneComponent.get();
+	}
+}
+
+
+FTransform& AActor::GetActorTransform()
+{
+	return RootComponent->Transform;
+}
+
+void AActor::SetActorTransform(const FTransform& _Transform)
+{
+	RootComponent->Transform = _Transform;
+}
+
+FVector AActor::GetActorForwardVector()
+{
+	return RootComponent->Transform.GetForward();
+}
+
+FVector AActor::GetActorRightVector()
+{
+	return RootComponent->Transform.GetRight();
+}
+FVector AActor::GetActorUpVector()
+{
+	return RootComponent->Transform.GetUp();
+}
+
+FVector AActor::GetActorLocation()
+{
+	return RootComponent->Transform.GetPosition();
+}
+
+void AActor::SetActorLocation(FVector _Value)
+{
+	RootComponent->Transform.SetPosition(_Value);
+}
+
+void AActor::SetActorScale3D(FVector _Value)
+{
+	RootComponent->Transform.SetScale(_Value);
+}
+
+void AActor::SetActorRotation(FVector _Value)
+{
+	RootComponent->Transform.SetRotationDeg(_Value);
+}
+
+void AActor::AddActorLocation(FVector _Value)
+{
+	RootComponent->Transform.AddPosition(_Value);
+}
+
+void AActor::AddActorRotation(FVector _Value)
+{
+	RootComponent->Transform.AddRotationDeg(_Value);
+}
+
+void AActor::AddActorScale3D(FVector _Value)
+{
+	RootComponent->Transform.AddScale(_Value);
+}
+
+
+// 액터에서만 인풋 처리가 가능하게 되었습니다.
+bool AActor::IsDoubleClick(int _Key, float _ClickTime)
+{
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsDoubleClick(_Key, _ClickTime);
+}
+
+void AActor::OnlyInput(AActor* _this)
+{
+	PrevInputActors = InputActors;
+	InputActors.clear();
+	InputActors.insert(_this);
+}
+
+void AActor::OnlyInputStop()
+{
+	InputActors.clear();
+	InputActors = PrevInputActors;
+}
+
+bool AActor::IsDown(int _Key)
+{
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsDown(_Key);
+}
+
+float AActor::GetPressTime(int _Key)
+{
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::GetPressTime(_Key);
+
+}
+
+bool AActor::IsPress(int _Key)
+{
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsPress(_Key);
+
+}
+
+bool AActor::IsUp(int _Key)
+{
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsUp(_Key);
+
+}
+
+bool AActor::IsFree(int _Key) {
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsFree(_Key);
+
+}
+
+bool AActor::IsAnykeyDown() {
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsAnykeyDown();
+
+}
+
+bool AActor::IsAnykeyPress() {
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsAnykeyPress();
+
+}
+
+bool AActor::IsAnykeyUp()
+{
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsAnykeyUp();
+
+}
+
+bool AActor::IsAnykeyFree()
+{
+	// InputActors 등록된 애들만 입력을 받을수 있다.
+	if (false == InputActors.contains(this))
+	{
+		return false;
+	}
+
+	return UEngineInput::IsAnykeyFree();
+
+}
---
+++ DirectX_KatanaZero/EngineCore/Actor.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <EngineBase/Transform.h>
+
+#include <vector>
+#include "TickObject.h"
+#include "WorldObject.h"
+#include "Level.h"
+
+
+// 설명 : 액터가 트랜스폼을 가지는게 아니라
+class ULevel;
+class UActorComponent;
+class USceneComponent;
+class AActor : public UTickObject, public UWorldObject
+{
+	friend ULevel;
+	GENERATED_BODY(UTickObject)
+
+public:
+	// constrcuter destructer
+	AActor();
+	~AActor();
+
+	// delete Function
+	AActor(const AActor& _Other) = delete;
+	AActor(AActor&& _Other) noexcept = delete;
+	AActor& operator=(const AActor& _Other) = delete;
+	AActor& operator=(AActor&& _Other) noexcept = delete;
+
+	// 오로지 생성자에서만 호출될수 있게 만들어 놓는다.
+	// 언리얼 따라할려고 그냥 함.
+	template<typename ComponentType>
+	ComponentType* CreateDefaultSubObject(std::string_view _Name)
+	{
+		if (false == ULevel::IsActorConstructer)
+		{
+			MsgBoxAssert("언리얼에서는 생성자에서밖에 컴포넌트를 생성할수 없습니다.");
+		}
+
+		std::shared_ptr<UActorComponent> NewComponent = std::make_shared<ComponentType>();
+
+		PushComponent(NewComponent, _Name);
+
+		return dynamic_cast<ComponentType*>(NewComponent.get());
+	}
+
+	FTransform& GetActorTransform();
+	void SetActorTransform(const FTransform& _Transform);
+
+	FVector GetActorLocation();
+	FVector GetActorForwardVector();
+	FVector GetActorRightVector();
+	FVector GetActorUpVector();
+
+	void SetActorScale3D(FVector _Value);
+	void SetActorRotation(FVector _Value);
+	void SetActorLocation(FVector _Value);
+
+	void AddActorScale3D(FVector _Value);
+	void AddActorRotation(FVector _Value);
+	void AddActorLocation(FVector _Value);
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+	// 언리얼이 왜 액터가 트랜스폼을 가지지 못하게 했을까?
+	// GameMode 기능적인 면에서 트랜스폼이 필요 없는 애들도 많아요.
+	// 액터란 이 어떠한 레벨에서 어떠한 역할을 해주시면 하면 된다.
+	// 거기에 필요한 기능만 만들면 되기 때문에 
+	// 컴포넌트 구조라는 것.
+	// 뭘만들든 동적으로 만들수 있는 구조를 만드는것.
+	USceneComponent* RootComponent = nullptr;
+	std::vector<std::shared_ptr<UActorComponent>> Components;
+
+	void PushComponent(std::shared_ptr<UActorComponent> _Component, std::string_view _Name);
+	void RootCheck();
+
+	/////////////////////// 인풋
+
+
+
+
+public:
+	void InputOff();
+	void InputOn();
+
+	// 이녀석을 쓰면
+	// UEngineInput <= 항상동작 키누르면
+
+	// 액터에서만 인풋 처리가 가능하게 되었습니다.
+	bool IsDoubleClick(int _Key, float _ClickTime);
+	bool IsDown(int _Key);
+	float GetPressTime(int _Key);
+	bool IsPress(int _Key);
+	bool IsUp(int _Key);
+	bool IsFree(int _Key);
+	bool IsAnykeyDown();
+	bool IsAnykeyPress();
+	bool IsAnykeyUp();
+	bool IsAnykeyFree();
+
+	static void OnlyInput(AActor* _this);
+	static void OnlyInputStop();
+
+private:
+	// set은 굉장히 간단한 자료구조로서
+	// Value 없는 맵입니다.
+	static std::set<AActor*> InputActors;
+	static std::set<AActor*> PrevInputActors;
+
+
+};
+
