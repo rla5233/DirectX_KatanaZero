166b1c095325ea011db18af95d0b7560549982a3
rla5233 <rla5233@naver.com>
2024-04-08 21:05:42 +0900

    김수원 : EngineBase - Debug 파일 추가

---
+++ DirectX_KatanaZero/EngineBase/EngineDebug.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <assert.h>
+#include <Windows.h>
+#include <string>
+#include <string_view>
+
+namespace UEngineDebug
+{
+#define LeakCheck _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
+#define MsgBoxAssert(Value) \
+	std::string ErrorText = std::string(Value); \
+	MessageBoxA(nullptr, ErrorText.c_str(), "치명적 에러", MB_OK); assert(false);
+
+	void /*EngineDebug::*/OutPutDebugText(std::string_view _DebugText);
+}
+
