637fe27928b05ada359261041621539b8c690be1
rla5233 <rla5233@naver.com>
2024-04-09 11:10:27 +0900

    김수원 : 프로젝트 종속성 설정 업데이트

--- DirectX_KatanaZero/KatanaZero_Core/TitleScreen.cpp
+++ DirectX_KatanaZero/KatanaZero_Core/TitleScreen.cpp
@@ -19,9 +19,9 @@ void ATitleScreen::BeginPlay()
 	Super::BeginPlay();
 
 	SetActorLocation({ 0.0f, 0.0f, 0.0f });
-	SetActorScale3D({ 1280.0f, 720.0f, 1.0f });
+	SetActorScale3D({ 1280.0f, 1440.0f, 1.0f });
 
-	BackGround->Transform.SetScale({ 1280.0f, 720.0f, 1.0f });
+	BackGround->Transform.SetScale({ 1280.0f, 1440.0f, 1.0f });
 
 }
 
