5a5661cb12c27bd38b073a55847835580d22be01
rla5233 <rla5233@naver.com>
2024-04-12 17:12:43 +0900

    김수원 : MouseAim - 업데이트

--- DirectX_KatanaZero/KatanaZero_Core/MouseAim.cpp
+++ DirectX_KatanaZero/KatanaZero_Core/MouseAim.cpp
@@ -34,6 +34,7 @@ void AMouseAim::StateInit()
 	// Start
 	State.SetStartFunction("Play", [=]
 		{
+			GEngine->EngineWindow.CursorOff();
 			Aim->SetActive(true);
 		}
 	);
@@ -45,13 +46,10 @@ void AMouseAim::StateInit()
 			FVector MousePos = GEngine->EngineWindow.GetScreenMousePos();
 
 			FVector WindowScale = GEngine->EngineWindow.GetWindowScale();
-			FVector TargetPos =
-			FVector(CameraPos.X, CameraPos.Y, 0.f) +
-			FVector(MousePos.X - WindowScale.hX(), -(MousePos.Y - WindowScale.hY()), 200.f);
+			FVector TargetPos =	FVector(CameraPos.X, CameraPos.Y, 0.f) + FVector(MousePos.X - WindowScale.hX(), -(MousePos.Y - WindowScale.hY()), 200.f);
 			SetActorLocation(TargetPos);
 		}
 	);
-
 }
 
 void AMouseAim::Tick(float _DeltaTime)
