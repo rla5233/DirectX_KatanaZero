7f6c0206cf0ebb6dd07b4e87d7353da845edf4bd
rla5233 <rla5233@naver.com>
2024-05-10 19:04:10 +0900

    김수원 : 업데이트

--- DirectX_KatanaZero/KatanaZero_Core/PlayLevelBase_State.cpp
+++ DirectX_KatanaZero/KatanaZero_Core/PlayLevelBase_State.cpp
@@ -243,6 +243,8 @@ void APlayLevelBase::Replay(float _DeltaTime)
 	if (true == IsDown(VK_RBUTTON))
 	{
 		InputOff();
+		USoundManager::GetInst()->GetReplayPlay().Off();
+		USoundManager::GetInst()->GetReplayFastPlay().Off();
 		State.ChangeState(PlayLevelState::transition_on);
 		return;
 	}
